5f47337d346a5d29c88fd7a0fad563cff9e9f2e3
salif <tanksalif@gmail.com>
2025-09-30 06:07:52 -0400

    Update dependencies and metro config

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add support for image files
+config.resolver.assetExts.push("jpg", "jpeg", "png", "gif", "webp", "svg");
+
 module.exports = withNativeWind(config, { input: "./global.css" });
